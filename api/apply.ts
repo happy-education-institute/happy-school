79b235930bc6ed75bf081a8615304a30ecb4e5c9
Anshul Gadia <anshulgadia04@gmail.com>
2026-06-13 17:46:08 +0530

    redeploy

--- api/apply.ts
+++ api/apply.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
+
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
